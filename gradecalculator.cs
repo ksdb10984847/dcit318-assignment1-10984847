0d1fd8b0d33ba8364187af169808e3269927d296
ksdb10984847 <102569074+ksdb10984847@users.noreply.github.com>
2024-06-02 23:27:01 +0000

    Add files via upload
    oi

---
+++ gradecalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numerical grade between 0 and 100:");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Letter Grade: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Letter Grade: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Letter Grade: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Letter Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Letter Grade: F");
+            }
+        }
+    }
+}
