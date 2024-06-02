d3271ad09955b25e56ecfb5f0630010452d7e709
ksdb10984847 <102569074+ksdb10984847@users.noreply.github.com>
2024-06-02 23:28:42 +0000

    Add files via upload
    .

---
+++ ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+
+            Console.WriteLine("Ticket Price: GHC" + ticketPrice);
+        }
+    }
+}
